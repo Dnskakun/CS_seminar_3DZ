76bf9fe249ce0de10ba6b53160fc28a55320b431
dnskakun <dimon_sdn@list.ru>
2022-12-26 22:51:54 +0300

    Решение задачи №3

---
+++ Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿//Программа принимает на вход число N и выдает таблицу кубов чисел от 1 до N
+
+int userInput1 = UserInput();
+
+if(userInput1 > 0)
+{
+    int count = 1;
+    while (count < userInput1)
+    {
+        Console.Write($"{Math.Pow(count, 3)}, ");
+        count++;
+    }
+    Console.Write($"{Math.Pow(userInput1, 3)}.");
+}
+else
+{
+    Console.WriteLine("Введенно число должно быть больше нуля. Попробуйте ещё раз!");
+}
+
+
+//Методы
+int UserInput()
+{
+    int number;
+    Console.Write("Введите целое число больше нуля: ");
+    while (true)
+    {
+        if(int.TryParse(Console.ReadLine(), out number))
+        {
+        break;
+        }
+        Console.WriteLine("Ошибка ввода!");
+    }
+    return number;
+}
